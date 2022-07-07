3499990171890d00d8bd31a5d450fa6ebfec8310
Serita <serita@star-career.co.jp>
2022-07-07 17:16:59 +0900

    create test code

--- docs/src/components/App.jsx
+++ docs/src/components/App.jsx
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [ count, setCount ] = useState(0);
+
+  const addCount = () => {
+    setCount(count + 1);
+  }
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Hello Word</h1>
+      <p>{count}</p>
+      <button onClick={addCount}>+</button>
     </div>
   );
 }
